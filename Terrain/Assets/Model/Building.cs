380c1a4fb90521486b3b59c4183827bfc35f9886
Joshua Kim <jkim682@aucklanduni.ac.nz>
2019-10-02 12:00:32 +1300

    remove buildable interface

--- Terrain/Assets/Model/Building.cs
+++ Terrain/Assets/Model/Building.cs
@@ -2,7 +2,92 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-abstract class Building
+public abstract class Building
 {
+    float initialMoney;
+
+    public float InitialMoney
+    {
+        get
+        {
+            return initialMoney;
+        }
+        set
+        {
+            initialMoney = value;
+        }
+    }
+
+    float initialGreen;
+
+    public float InitialGreen
+    {
+        get
+        {
+            return initialGreen;
+        }
+        set
+        {
+            initialGreen = value;
+        }
+    }
+
+    float initialHappiness;
+
+    public float InitialHappiness
+    {
+        get
+        {
+            return initialHappiness;
+        }
+        set
+        {
+            initialHappiness = value;
+        }
+    }
+
+    float generateMoney;
+
+    public float GenerateMoney
+    {
+        get
+        {
+            return generateMoney;
+        }
+        set
+        {
+            generateMoney = value;
+        }
+    }
+
+    float generateGreen;
+
+    public float GenerateGreen
+    {
+        get
+        {
+            return generateGreen;
+        }
+        set
+        {
+            generateGreen = value;
+        }
+    }
+
+    float generateHappiness;
+
+    public float GenerateHappiness
+    {
+        get
+        {
+            return generateHappiness;
+        }
+        set
+        {
+            generateHappiness = value;
+        }
+    }
+
+
 
 }
