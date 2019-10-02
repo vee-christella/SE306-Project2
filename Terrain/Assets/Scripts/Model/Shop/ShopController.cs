8bfafe66097db174635e29d021e5dd55b3ef3deb
Edward Zhang <ezha731@aucklanduni.ac.nz>
2019-10-03 12:01:38 +1300

    Began shop controller and shop UI

---
+++ Terrain/Assets/Scripts/Model/Shop/ShopController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopController : MonoBehaviour
+{
+
+    public GameObject Shop;
+   public void OpenShop(){
+       Shop.SetActive(true);
+       Debug.Log("clicked");
+       Time.timeScale = 1;
+   }
+
+   public void CloseShop(){
+       Shop.SetActive(false);
+       Time.timeScale = 1;
+   }
+}
