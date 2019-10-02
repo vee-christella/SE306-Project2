0103a4cde586b0957913364b1977e3c81eda4952
Tyger Kong <tyger.kong@hotmail.com>
2019-10-02 21:01:54 +1300

    Main  and Game scene now linked

--- Terrain/Assets/Scripts/Menu/MainMenuController.cs
+++ Terrain/Assets/Scripts/Menu/MainMenuController.cs
@@ -7,7 +7,7 @@ public class MainMenuController : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("GameScene");
 
     }
 
