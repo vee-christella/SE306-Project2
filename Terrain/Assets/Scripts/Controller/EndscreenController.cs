247e00790a03b83bad4e1b11da5f6404f4b335dd
Dinith1 <wannigamadinith@gmail.com>
2019-10-06 16:22:32 +1300

    added end screen

---
+++ Terrain/Assets/Scripts/Controller/EndscreenController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndscreenController : MonoBehaviour
+{
+    public GameObject endScreenUI;
+
+    void update()
+    {
+        
+    }
+}
