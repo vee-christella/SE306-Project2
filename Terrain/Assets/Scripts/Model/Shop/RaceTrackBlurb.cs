03713200371ff1d778ba3d40ca303a64bfff566c
Edward Zhang <ezha731@aucklanduni.ac.nz>
2019-10-06 19:21:34 +1300

    Added blurb display in shop

---
+++ Terrain/Assets/Scripts/Model/Shop/RaceTrackBlurb.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class RaceTrackBlurb : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Text blurb;
+    public GameObject blurbObject;
+    public GameObject panel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RaceTrack raceTrack = new RaceTrack();
+        blurb.text = raceTrack.Blurb;
+        panel.SetActive(false);
+        blurbObject.SetActive(false);
+    }
+    
+    public void OnPointerEnter(PointerEventData pointerEventData){
+        panel.SetActive(true);
+        blurbObject.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData pointerEventData){
+        panel.SetActive(false);
+        blurbObject.SetActive(false);
+    }
+}
