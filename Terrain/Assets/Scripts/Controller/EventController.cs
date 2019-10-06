ab914913e98b9bc47609258b9bde83b6658c8dc8
Vanessa Ciputra <vee.christella@gmail.com>
2019-10-07 12:20:47 +1300

    Added effect images and changes

--- Terrain/Assets/Scripts/Controller/EventController.cs
+++ Terrain/Assets/Scripts/Controller/EventController.cs
@@ -10,12 +10,20 @@ public class EventController : MonoBehaviour
     public GameObject eventPopupPanel;
 
     public TextMeshProUGUI eventInfo;
+    public TextMeshProUGUI moneyEffect;
+    public TextMeshProUGUI greenPointsEffect;
+    public TextMeshProUGUI happinessEffect;
 
     public void DisplayPopup(Event gameEvent)
     {
-        eventInfo.text = "Event occurance:  " + gameEvent.GetType().Name.ToString() + "\n" +
-           "Effects are: Minus " + gameEvent.GreenPointDelta + " green points, " + gameEvent.MoneyDelta + " money points " +
-           " and " + gameEvent.HappinessDelta + " happiness points";
+        eventInfo.text = "A  " + gameEvent.GetType().Name.ToString() + " has occurred! \n";
+
+        moneyEffect.text = DisplayEffect(gameEvent.MoneyDelta) + gameEvent.MoneyDelta;
+        greenPointsEffect.text = DisplayEffect(gameEvent.GreenPointDelta) + gameEvent.GreenPointDelta;
+        happinessEffect.text = DisplayEffect(gameEvent.HappinessDelta) + gameEvent.GreenPointDelta;
+
+
+
         Debug.Log("pop up");
         eventPopupPanel.SetActive(true);
     }
@@ -24,5 +32,19 @@ public class EventController : MonoBehaviour
     {
         eventPopupPanel.SetActive(false);
     }
+
+    // Configure which sign to display in the random event section
+    private string DisplayEffect(int effect)
+    {
+        if (effect < 0)
+        {
+            return "- ";
+        }
+        else
+        {
+            return "+ ";
+        }
+    }
 }
+
 
