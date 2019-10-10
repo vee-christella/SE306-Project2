3684c18ee3d17230523fda21a3c42d7d86d10149
Michelle Tan <michellemtan@outlook.com>
2019-10-10 13:29:43 +1300

    Finalised event blurbs

--- Terrain/Assets/Scripts/Model/Events/Heatwave.cs
+++ Terrain/Assets/Scripts/Model/Events/Heatwave.cs
@@ -9,6 +9,7 @@ public class Heatwave : Event
     public Heatwave(Game game) : base(-5, -1, -5)
     {
         this.Type = EventType.Random;
+        this.Description = "Rising temperatures are affecting wildlife and their habitats. As temperatures change, many species are on the move. Some butterflies, foxes, and alpine plants have migrated farther north or to higher, cooler areas.";
         this.Game = game;
     }
 
