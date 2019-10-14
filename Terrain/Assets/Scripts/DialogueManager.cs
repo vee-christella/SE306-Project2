3955dd099777fa614fa80c79003fc88fc08a5b52
Vanessa Ciputra <vee.christella@gmail.com>
2019-10-14 14:59:24 +1300

    Create Dialogue

---
+++ Terrain/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> conversation;
+
+    public GameObject continueButton;
+    public GameObject startButton;
+
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+
+    private void Start()
+    {
+        conversation = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        Debug.Log("Start convo");
+
+        nameText.text = dialogue.npcName;
+
+        conversation.Clear();
+
+        foreach(string sentence in dialogue.sentences)
+        {
+            conversation.Enqueue(sentence);
+        }
+
+
+
+        DisplayNextSentence();
+
+        startButton.SetActive(false);
+        continueButton.SetActive(true);
+
+    }
+
+
+    public void DisplayNextSentence()
+    {
+        if (conversation.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = conversation.Dequeue();
+        dialogueText.text = sentence;
+
+    }
+
+    public void EndDialogue()
+    {
+        Debug.Log("Ended convo");
+    }
+}
