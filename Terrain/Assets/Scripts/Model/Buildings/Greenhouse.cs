1120a70495da0c6535aceeae7449ee668ff0d258
Tyger Kong <40849970+WaiHaiDere@users.noreply.github.com>
2019-10-15 16:59:37 +1300

    Revert "Merge pull request #65 from vee-christella/feature/add-to-shop"
    This reverts commit 3dcc529f14b865d4ad01ac235e4b159578cfa945.

--- Terrain/Assets/Scripts/Model/Buildings/Greenhouse.cs
+++ Terrain/Assets/Scripts/Model/Buildings/Greenhouse.cs
@@ -6,7 +6,6 @@ public class Greenhouse : Building
     public Greenhouse() : base(8, -80, 30, 0, 20, -10, 0, 3)
     {
         this.TypeOfBuilding = BuildingType.Utility;
-        this.Name = "Greenhouse";
         this.Blurb = "Greenhouses are low carbon structures that are used to grow crops in a controlled environment. " +
             "Growing plants in a greenhouse allows for the growing season to be extended, particularly for plants " +
             "that do not thrive in winter. Recent technological changes have allowed for greenhouses to reduce carbon emissions by 75%.";
