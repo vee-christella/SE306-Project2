a5e1d8a5a44843d0c251198598a2646f25342626
Dinith1 <wannigamadinith@gmail.com>
2019-10-20 17:21:13 +1300

    prevented camera movements when mouse is over UI elements

--- Terrain/Assets/Scripts/Controller/CameraController.cs
+++ Terrain/Assets/Scripts/Controller/CameraController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 /*
 Retrieved from https://answers.unity.com/questions/666905/in-game-camera-movement-like-editor.html
@@ -29,40 +30,48 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
-        // Look around with Right Mouse
-        if (Input.GetMouseButton(1))
+        // Only allow mouse navigation when the cursor is not over a UI element
+        if (!EventSystem.current.IsPointerOverGameObject())
         {
-            Vector3 rotation = transform.eulerAngles;
-            pitch = rotation.x - lookSpeedV * Input.GetAxis("Mouse Y");
-            yaw = rotation.y + lookSpeedH * Input.GetAxis("Mouse X");
+            // Look around with Right Mouse click
+            if (Input.GetMouseButton(1))
+            {
+                Vector3 rotation = transform.eulerAngles;
+                pitch = rotation.x - lookSpeedV * Input.GetAxis("Mouse Y");
+                yaw = rotation.y + lookSpeedH * Input.GetAxis("Mouse X");
 
-            transform.eulerAngles = new Vector3(pitch, yaw, rotation.z);
+                transform.eulerAngles = new Vector3(pitch, yaw, rotation.z);
+            }
+
+            // Zoom in and out with Mouse Wheel
+            if ((transform.position.y > 1 && Input.GetAxis("Mouse ScrollWheel") > 0) || (transform.position.y < 15 && Input.GetAxis("Mouse ScrollWheel") < 0))
+            {
+                transform.Translate(0, 0, Input.GetAxis("Mouse ScrollWheel") * zoomSpeed, Space.Self);
+            }
         }
 
-        // Move camera forward/back/left/right with wasd respectively
+        // Move camera forward/back/left/right with w/a/s/d keys respectively
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
         {
+            // Set the direction of movement
             if (Input.GetKey(KeyCode.W))
             {
                 transform.TransformDirection(Vector3.forward);
-
             }
             else if (Input.GetKey(KeyCode.S))
             {
                 transform.TransformDirection(Vector3.back);
-
             }
             else if (Input.GetKey(KeyCode.A))
             {
                 transform.TransformDirection(Vector3.left);
-
             }
             else if (Input.GetKey(KeyCode.D))
             {
                 transform.TransformDirection(Vector3.right);
-
             }
 
+            // Set the distance of movement
             Vector3 forwardback = transform.forward * Input.GetAxis("Vertical") * wasdSpeed;
             Vector3 leftright = transform.right * Input.GetAxis("Horizontal") * wasdSpeed;
             forwardback.y = 0;
@@ -82,12 +91,6 @@ public class CameraController : MonoBehaviour
             Vector3 rotation = transform.eulerAngles;
             rotation.y += rotateSpeed * Time.deltaTime;
             transform.eulerAngles = rotation;
-        }
-
-        // Zoom in and out with Mouse Wheel
-        if ((transform.position.y > 1 && Input.GetAxis("Mouse ScrollWheel") > 0) || (transform.position.y < 15 && Input.GetAxis("Mouse ScrollWheel") < 0))
-        {
-            transform.Translate(0, 0, Input.GetAxis("Mouse ScrollWheel") * zoomSpeed, Space.Self);
         }
     }
 
